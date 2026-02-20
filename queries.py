4dbc6884401c6ffb89fe0b0345bb41bbc1bf0460
“kaiohs333” <kaiohsilveira@gmail.com>
2026-02-20 20:27:05 -0300

    add .gitignore and initial GraphQL query for repositories

---
+++ queries.py
@@ -0,0 +1,29 @@
+# Criamos uma variável constante (em letras maiúsculas) para guardar a string
+QUERY_100_REPOS = """
+{
+  search(query: "stars:>10000 sort:stars-desc", type: REPOSITORY, first: 100) {
+    nodes {
+      ... on Repository {
+        nameWithOwner
+        createdAt
+        pullRequests(states: MERGED) {
+          totalCount
+        }
+        releases {
+          totalCount
+        }
+        updatedAt
+        primaryLanguage {
+          name
+        }
+        issues {
+          totalCount
+        }
+        closedIssues: issues(states: CLOSED) {
+          totalCount
+        }
+      }
+    }
+  }
+}
+"""
